b2f0add1e4a1d1adcc9c1745ce37eb1fb2b940c2
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2026-02-04 13:45:45 -0300

    feat(android): assets para android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.matiasgaleano.foodly-notes',
+  appId: 'io.matiasgaleano.foodlynotes',
   appName: 'Foodly Notes',
   webDir: 'www',
 };
